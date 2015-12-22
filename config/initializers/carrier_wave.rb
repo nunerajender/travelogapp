dac63dd80c4e39dabbdc3f3ca4af9ba334e02b75
Siva <siva@Xus-MacBook-Pro.local>
2015-12-22 11:35:42 +0800

    Git imageMagick on mac

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,27 +1,27 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJXAS6QC5PAV3OABA',
-    :aws_secret_access_key  => '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',
-    :region                 => 'us-west-2'
-  }
-  config.fog_directory  = 'usebento'
-  config.fog_public = false
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}"}
-end
-
-
-
 # CarrierWave.configure do |config|
 #   config.fog_provider = 'fog/aws'                        # required
 #   config.fog_credentials = {
 #     :provider               => 'AWS',
-#     :aws_access_key_id      => 'AKIAIM2IARER7FIT4EDQ',
-#     :aws_secret_access_key  => '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',
-#     :region                 => 'Oregon'
+#     :aws_access_key_id      => 'AKIAJXAS6QC5PAV3OABA',
+#     :aws_secret_access_key  => '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',
+#     :region                 => 'us-west-2'
 #   }
-#   config.fog_directory  = 'travelog'
+#   config.fog_directory  = 'usebento'
 #   config.fog_public = false
 #   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}"}
 # end
+
+
+
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAIM2IARER7FIT4EDQ',
+    :aws_secret_access_key  => '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',
+    :region                 => 'us-west-2'
+  }
+  config.fog_directory  = 'travelog'
+  config.fog_public = false
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}"}
+end
